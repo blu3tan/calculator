4232644403e3ebdbbee00efdde84329ae8b923e9
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-29 17:58:45 +0100

    Build plus operator logic
    The operation is displayed correctly but not yet executed

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
 
 const topScreen = document.getElementById('top-screen');
 const bottomScreen = document.getElementById('bottom-screen');
-// const btnOne = document.getElementById('one');
 const numberKeys = document.querySelectorAll('#num');
+const operatorKeys = document.querySelectorAll('.operator');
+const cKey = document.getElementById('c-operator');
+const delKey = document.getElementById('del-operator');
+const plusOperator = document.getElementById('plus-operator');
+const minusOperator = document.getElementById('min-operator');
+const multiplyOperator = document.getElementById('mult-operator');
+const divideOperator = document.getElementById('div-operator');
 
-// btnOne.addEventListener('click', () => {
-//     bottomDisplay += '1';
-//     bottomScreen.textContent += bottomDisplay;
-//     bottomDisplay = '';
-// });
-
-let firstNum;
-let secondNum;
-let operator;
+let operating = false;
+let firstNum = 0;
+let secondNum = 0;
+let numbers = [];
+let operator = '';
 let topDisplay;
-let bottomDisplay = '';
 
+cKey.addEventListener('click', () => {
+    firstNum = 0;
+    bottomScreen.textContent = '';
+    topScreen.textContent = '';
+});
+delKey.addEventListener('click', () => {
+    bottomScreen.textContent = bottomScreen.textContent.slice(0, -1);
+})
 
-function addListenerToNumbers (numberKeys) {
-    numberKeys.forEach(element => {
+plusOperator.addEventListener('click', () => {
+    operating = true;
+    numbers.push(+(bottomScreen.textContent));
+    topScreen.textContent = (bottomScreen.textContent + '+');
+})
+
+// Add listener to each number key, on click the proper number is displayed and stored
+function addListenerToNumbers (array) {
+    array.forEach(element => {
         element.addEventListener('click', () => {
-            bottomDisplay += element.innerText;
-            bottomScreen.textContent += bottomDisplay;
-            bottomDisplay = '';
+            if (!operating) {
+            bottomScreen.textContent += element.innerText;
+            }
+            else {
+                operating = false;
+                bottomScreen.textContent = '';
+                bottomScreen.textContent += element.innerText;
+            }
         })
     });
 };
+// function addListenerToOperators (array) {
+//     array.forEach(element => {
+//         element.addEventListener('click', () => {
+//             operatorRoutine(element);
+//         })
+//     });
+// };
+
+// function operatorRoutine (element) {
+//     operator = element.innerText;
+//     firstNum += +(bottomScreen.textContent);
+//     bottomScreen.textContent += element.innerText;
+// }
+
+
+addListenerToNumbers(numberKeys);
+
+
 
 
 function add (num1, num2) {
@@ -43,5 +82,3 @@ function divide (num1, num2) {
 function operate (num1, num2, operator) {
     return operator(num1, num2);
 }
-
-addListenerToNumbers(numberKeys);
