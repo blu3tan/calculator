2b8f3b69b3bd3de485a124638d95a9910e42b071
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-29 14:40:41 +0100

    Build function to add listener to buttons
    Each button on click add his number to the display and to the variable

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 
+const topScreen = document.getElementById('top-screen');
+const bottomScreen = document.getElementById('bottom-screen');
+// const btnOne = document.getElementById('one');
+const numberKeys = document.querySelectorAll('#num');
+
+// btnOne.addEventListener('click', () => {
+//     bottomDisplay += '1';
+//     bottomScreen.textContent += bottomDisplay;
+//     bottomDisplay = '';
+// });
+
 let firstNum;
 let secondNum;
 let operator;
+let topDisplay;
+let bottomDisplay = '';
+
+
+function addListenerToNumbers (numberKeys) {
+    numberKeys.forEach(element => {
+        element.addEventListener('click', () => {
+            bottomDisplay += element.innerText;
+            bottomScreen.textContent += bottomDisplay;
+            bottomDisplay = '';
+        })
+    });
+};
+
 
 function add (num1, num2) {
     return num1 + num2;
@@ -19,4 +44,4 @@ function operate (num1, num2, operator) {
     return operator(num1, num2);
 }
 
-console.log(operate(5, 2, divide));
+addListenerToNumbers(numberKeys);
