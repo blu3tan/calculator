532215610d7a72a4b9e7529d3e376a05b17c43d2
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-31 22:13:30 +0100

    Fix bug on * and / operators
    Added provideInitial() function to avoid lack of initial value

--- script.js
+++ script.js
@@ -13,15 +13,16 @@ const divideOperator = document.getElementById('div-operator');
 const floatKey = document.getElementById('dot');
 const total = document.getElementById('total');
 
-const add = (num1, num2) => num1 += num2;
-const subtract = (num1, num2) => num1 -= num2; // <= problem here
-const multiply = (num1, num2) => num1 *= num2; // <= problem here
-const divide = (num1, num2) => num1 /= num2;
 
 let operator = [];
 let partial = [];
 let numbers = [];
 let numString = '';
+
+const add = (num1, num2) => num1 += num2;
+const subtract = (num1, num2) => num1 -= num2;
+const multiply = (num1, num2) => num1 *= num2;
+const divide = (num1, num2) => num1 /= num2;
 
 // Special keys custom listeners
 cKey.addEventListener('click', () => {
@@ -35,6 +36,7 @@ cKey.addEventListener('click', () => {
 });
 delKey.addEventListener('click', () => {
     bottomScreen.textContent = bottomScreen.textContent.slice(0, -1);
+    numString = numString.slice(0, -1);
 });
 
 floatKey.addEventListener('click', (e) => {
@@ -45,24 +47,28 @@ floatKey.addEventListener('click', (e) => {
 });
 
 plusOperator.addEventListener('click', () => {
+    clickBlock(operatorKeys);
     bottomScreen.textContent += '+';
     operator.push(add);
     calculateAndDisplay();
 });
 
 minusOperator.addEventListener('click', () => {
+    clickBlock(operatorKeys);
     bottomScreen.textContent += '-';
     operator.push(subtract);
     calculateAndDisplay();
 });
 
 multiplyOperator.addEventListener('click', () => {
+    clickBlock(operatorKeys);
     bottomScreen.textContent += 'x';
     operator.push(multiply);
     calculateAndDisplay();
 });
 
 divideOperator.addEventListener('click', () => {
+    clickBlock(operatorKeys);
     bottomScreen.textContent += '÷';
     operator.push(divide);
     calculateAndDisplay();
@@ -71,13 +77,15 @@ divideOperator.addEventListener('click', () => {
 total.addEventListener('click', () => {
     numbers.push(+numString);
     numString = '';
-    partial = numbers.reduce(operator[0]);
+    // partial = numbers.reduce(operator[0]);
+    provideInitial();
     operator = [];
     numbers.splice(0);
     numbers.push(partial);
     bottomScreen.textContent = partial;
     topScreen.textContent = '';
     clickBlock(numberKeys);
+    removeClickBlock(operatorKeys);
 });
 
 //  Main operation function - perform the operation based on the current operator
@@ -85,7 +93,8 @@ total.addEventListener('click', () => {
 function calculateAndDisplay () {
     numbers.push(+numString);
     numString = '';
-    partial = numbers.reduce(operator[0]);
+    // partial = numbers.reduce(operator[0]);
+    provideInitial();
     checkOperator();
     topScreen.textContent = partial;
     numbers.splice(0);
@@ -101,12 +110,22 @@ function checkOperator () {
     };
 };
 
+function provideInitial () {
+    if ((operator[0] == divide || operator[0] == multiply) && 
+        (numbers[1] == 0 || !numbers[1])) {
+        numbers[1] = 1;
+        partial = numbers.reduce(operator[0]);
+    }
+    else partial = numbers.reduce(operator[0]);
+};
+
 // Add listener to each number key, on click the proper number is displayed and stored
 function addListenerToNumbers (array) {
     array.forEach(element => {
         element.addEventListener('click', () => {
                 numString += (element.innerText);
                 bottomScreen.textContent += element.innerText;
+                removeClickBlock(operatorKeys);
                 checkLength();
             })
     });
